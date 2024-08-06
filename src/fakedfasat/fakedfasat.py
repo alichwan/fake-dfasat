81abfe97169b51163afeaf759718107faf1e1a19
Alichwan <all.each.one@gmail.com>
2024-08-05 23:03:46 -0400

    Fix of typos, docstring and default arguments

--- src/fakedfasat/fakedfasat.py
+++ src/fakedfasat/fakedfasat.py
@@ -6,20 +6,28 @@ from src.fakedfasat.sat_encoding import SATEncoding
 
 
 def fakedfasat(
-    sample: Dict[str, List], lower_bound: int = 1, upper_bound: int = 5
+    sample: Dict[str, List],
+    lower_bound: int = 1,
+    upper_bound: int = 5,
+    solver: str = "g4",
 ) -> DFA:
     """Based in the algorithm 4 of Heule & Verwer (2013) but not considering
     the greedy algorithm (so no need of a bound m)
 
     Args:
         sample (Dict[str, List]): Group of traces to train the model
+        lower_bound (int, optional): Minimum amount of colors to try. Defaults to 1.
+        upper_bound (int, optional): Maximum amount of colors to try. Defaults to 5.
+        solver (str, optional): PySat solver name. Defaults to "g4" (Glucose 4).
 
+    Returns:
+        DFA: Automaton
     """
     apta = APTA(sample)
 
     for n_colors in range(lower_bound, upper_bound + 1):  # Incremental part
         apta_encoded = SATEncoding(apta, n_colors)
-        automaton = apta_encoded.solve()
+        automaton = apta_encoded.solve(solver_name=solver)
         if automaton is not None:
             return automaton
         # TODO: implement timeout
